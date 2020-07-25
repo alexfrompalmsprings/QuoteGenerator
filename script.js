4c4bb05c62c4feda1de4033cfd6c3ed14dc8fed6
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-25 11:34:37 -0700

    add logic for unknown autthors -- might refactor to ternary operator

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// GET QUOTE FORM API
+// all the elements neeed to manipulate the DOM
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+
+
+// GET QUOTE FORM API
 async function getQuote() {
   // This Proxy URL is here to make our API call in order to avoid a CORS error
   //##########################################################
@@ -7,12 +16,26 @@ async function getQuote() {
   //##########################################################
   const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
+
   try {
 
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
     console.log(data);
+    console.log(data.quoteAuthor);
+
+
+    // logic for an unknown author
+    if(data.quoteAuthor === ''){
+      authorText = 'Unknown'
+    } else{
+      authorText.innerText = data.quoteAuthor;
+
+    }
+    quoteText.innerText = data.quoteText;
+
   } catch (err) {
+    // getQuote()
     console.log('Dude you have an error man', err);
   }
 
@@ -20,3 +43,4 @@ async function getQuote() {
 
 // On load
 getQuote()
+
