a23387cc289fcfe98812594a68e5a948d1169ffe
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-25 12:30:19 -0700

    create div and style for the loader

--- script.js
+++ script.js
@@ -6,10 +6,26 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
+// show loading
+function loading(){
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
 
+//hide loading
+function complete(){
+  if(!loader.hidden){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // GET QUOTE FORM API
 async function getQuote() {
+
+  // add the loading function
+  loading()
+
   // This Proxy URL is here to make our API call in order to avoid a CORS error
   //##########################################################
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -41,6 +57,10 @@ async function getQuote() {
 
     quoteText.innerText = data.quoteText;
 
+    //fetching quote should be done by now; lets stop the loading
+    complete();
+
+
   } catch (err) {
     getQuote()
     console.log('Dude you have an error man', err);
@@ -65,4 +85,4 @@ newQuoteBtn.addEventListener('click', getQuote);
 
 // On load
 getQuote()
-
+loading()
